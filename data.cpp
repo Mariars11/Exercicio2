9cfb894a0ccc6505adeae83efdfa2789c45e7144
Maria Clara <70076859+Mariars11@users.noreply.github.com>
2022-03-30 11:19:49 -0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -13,42 +13,45 @@ int main(){
 	scanf("%d", &ano);
 	
 	if(dia < 1 || mes < 1 || mes >= 13 || ano < 1){
-		printf("Data invalida!");
+		printf("\nData invalida!");
 	}
 	//meses com 31 dias
 	if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && dia < 31 && dia >= 1 && mes >= 1 && mes < 13 && ano >= 1){
-			printf("Pr髕imo dia: %d/0%d/%d", dia + 1, mes, ano);
+			printf("\nPr贸ximo dia: %d/%d/%d", dia + 1, mes, ano);
 	}
 		else if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10) && dia == 31){
-				printf("Pr髕imo dia: 01/0%d/%d", mes + 1, ano);
+				printf("\nPr贸ximo dia: 01/%d/%d", mes + 1, ano);
 		}
 			else if(mes == 12 && dia == 31){
-				printf("Pr髕imo dia: 01/01/%d", ano + 1);
+				printf("\nPr贸ximo dia: 01/01/%d", ano + 1);
 			}	
 				
 	//fevereiro
 	if(mes == 2 && ((ano % 4) != 0) && dia >= 29 || mes == 2 && ((ano % 4) == 0) && dia > 29){
-		printf("Data invalida!");
+		printf("\nData invalida!");
 	}
 	else if(mes == 2 && dia < 28 && dia >= 1 && ano >= 1){
-		printf("Pr髕imo dia: %d/0%d/%d", dia + 1, mes, ano);
+		printf("\nPr贸ximo dia: %d/0%d/%d", dia + 1, mes, ano);
 	} 
 		else if(mes == 2 && dia >= 28 && (ano % 4) == 0 && dia < 29){
-			printf("Pr髕imo dia: %d/0%d/%d", dia + 1, mes, ano);
+			printf("\nPr贸ximo dia: %d/0%d/%d", dia + 1, mes, ano);
 		}
 			else if(mes == 2 && (ano % 4) == 0 && dia == 29){
-				printf("Pr髕imo dia: 01/0%d/%d", mes + 1, ano);
+				printf("\nPr贸ximo dia: 01/0%d/%d", mes + 1, ano);
 			}
 				else if(mes == 2 && dia == 28){
-					printf("Pr髕imo dia: 01/0%d/%d", mes + 1, ano);
+					printf("\nPr贸ximo dia: 01/0%d/%d", mes + 1, ano);
 				}
 	// mes com 30 dias			
 	if((mes == 4 || mes == 6 ||mes == 9 || mes == 11) && dia < 30 && dia >= 1 && mes >= 1 && mes < 13 && ano >= 1){
-			printf("Pr髕imo dia: %d/0%d/%d", dia + 1, mes, ano);
+			printf("\nPr贸ximo dia: %d/%d/%d", dia + 1, mes, ano);
 	}
 		else if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia == 30){
-				printf("Pr髕imo dia: 01/0%d/%d", mes + 1, ano);
+				printf("\nPr贸ximo dia: 01/%d/%d", mes + 1, ano);
 		}
+			else if((mes == 4 || mes == 6 || mes == 9 || mes == 11)&& dia >= 31){
+				printf("\nData invalida!");
+			} 
 	
 	return 0;
 }
